1773438b51ba3ea234ecb2a36cc46180d428d457
CPoku <Charlespokuaa1@gmail.com>
2020-01-23 11:30:00 +0100

    opdracht 1.3

---
+++ H02/vulmet.js
@@ -0,0 +1,7 @@
+document.getElementById("paginatitel").innerHTML = "Mijn gewelige pagina";
+
+document.getElementById("subkop").innerHTML = "Mijn gewelige subkop";
+
+document.getElementById("aap1").src = "img/Chino.jpg";
+
+document.getElementById("aap2").src = "img/Zarfnal.jpg";
