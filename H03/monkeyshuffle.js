6044a251af821c8946828aa71a62df4964510cd5
CPoku <Charlespokuaa1@gmail.com>
2020-01-29 10:20:43 +0100

    Opdracht 3.1

--- H03/monkeyshuffle.js
+++ H03/monkeyshuffle.js
@@ -1,4 +1,28 @@
 
 var plaatjes = document.getElementsByTagName("img");
-var random =  Math.floor(Math.random() *9 + 1);
-plaatjes[5].src = "../img/aap" + random + ".jpg"
+var random = 0;
+
+var randomNumbers = [];
+
+while (randomNumbers.length < 9 ) {
+    random =  Math.floor(Math.random() *9 + 1);
+    if (randomNumbers.lastIndexOf(random) == -1) {
+        randomNumbers.push(random);
+    }
+}
+
+random = 0;
+for (var plaatje in plaatjes) {
+    plaatjes[plaatje].src = "img/aap"+ randomNumbers[random] + ".jpg";
+    random++;
+
+}
+
+
+//for (var i = 0; i < plaatjes.length; i++ ) {
+// plaatjes[i].src = "img/aap" + random + ".jpg";
+// random = Math.floor(Math.random() * 9 + 1);
+//}
+
+
+
