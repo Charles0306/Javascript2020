c1fa383e0f43afcf8929e32e5655a223fda90ead
CPoku <Charlespokuaa1@gmail.com>
2020-02-01 23:58:50 +0100

    Opdracht 4.1

---
+++ H04/opdracht4.2/Imageslider.js
@@ -0,0 +1,11 @@
+var plaatjes = [1,2,3,4,5,6,7,8,9];
+
+var slideholder  = document.getElementById("slideholder");
+
+slideholder.style.backgroundImage = "url('img/aap1.jpg')";
+
+
+slideholder.addEventListener("click", function () {
+    slideholder.style.backgroundImage = "url('img/aap2.jpg')";
+
+})
