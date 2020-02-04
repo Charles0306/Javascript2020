ec3cdf10fa49d9dffd0f67def3d2048a3aeec408
CPoku <Charlespokuaa1@gmail.com>
2020-02-04 13:02:45 +0100

    Opdracht 4.3

--- H04/opdracht4.3/crookapp.js
+++ H04/opdracht4.3/crookapp.js
@@ -1,14 +1,30 @@
 var pictures = [1,2,3];
 var teller = 0;
 var picholder = document.getElementById("picholder");
+var picholder2 = document.getElementById("picholder2");
+var picholder3 = document.getElementById("picholder3");
+
 
 picholder.style.backgroundImage = "url('img/pokemonhead1.jpg')";
+picholder2.style.backgroundImage = "url('img/pokemonbody1.jpg')";
+picholder3.style.backgroundImage =  "url('img/pokemonbottom1.jpg')";
 
 
 picholder.addEventListener("click", function () {
     picholder.style.backgroundImage = "url('img/pokemonhead" + getPokemon() + ".jpg')";
 
 });
+
+picholder2.addEventListener("click", function () {
+    picholder2.style.backgroundImage = "url('img/pokemonbody" + getPokemon() + ".jpg')";
+});
+
+picholder3.addEventListener("click", function () {
+    picholder3.style.backgroundImage = "url('img/pokemonbottom" + getPokemon() + ".jpg')";
+
+});
+
+
 
 function getPokemon() {
     if (teller >= pictures.length) {
